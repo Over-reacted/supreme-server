d354afbaf1aff5466f0383b85fd6d80dbd950f0f
BorislavValov <borislav.valov1@gmail.com>
2021-03-04 13:35:45 +0200

    Add Product model

---
+++ src/modules/products/product.model.ts
@@ -0,0 +1,28 @@
+import { Schema, Document } from "mongoose";
+import {SchemaTypes} from "mongoose-double";
+
+/**
+ * Mongoose Profile Schema
+ */
+export const Product = new Schema({
+    name: { type: String, required: true },
+    slug: { type: String, required: true },
+    material: { type: String, required: true },
+    color: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: SchemaTypes.Double, required: true },
+    currency: { type: String, required: true },
+    image: { type: String, required: true },
+  });
+
+  export interface IProduct extends Document{
+    readonly _id: Schema.Types.ObjectId;
+    name: string;
+    slug: string;
+    material: string;
+    color: string;
+    description: string;
+    price: number;
+    currency: string;
+    image: string;
+  }
--- src/modules/products/products.repository.ts
+++ src/modules/products/products.repository.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { IProduct } from "./product.model";
 
 @Injectable()
 export class ProductsRepository {
-  constructor() {}
+  constructor(@InjectModel("Product") private readonly profileModel: Model<IProduct>) {}
 
   
 }
